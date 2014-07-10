21a0e2899b9cfb4a3fce588ef181538c2093876b
fouber <fouber.NET@gmail.com>
2014-07-10 21:31:12 +0800

    添加菜单栏

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,4 +1,3 @@
-
 exports.render = function(dom){
     // 使用__inline函数嵌入其他文件、图片。这里用作内嵌模板，
     // scrat已经配置了对handlebars后置的文件进行预编译，因此
@@ -32,6 +31,19 @@ exports.render = function(dom){
 
     data.height = data.items.length * 37;
 
-    //使用模板+数据得到html
+    // 使用模板+数据得到html
     dom.innerHTML = tpl(data);
+
+    // 绑定事件
+    var btn = document.getElementById('menu-switch');
+    btn.addEventListener('click', function(){
+        var list = document.getElementById('menu-list');
+        if(this.getAttribute('data-open') == 1){
+            list.style.height = 0;
+            this.setAttribute('data-open', '0');
+        } else {
+            list.style.height = data.height + 'px';
+            this.setAttribute('data-open', '1');
+        }
+    }, false);
 };
