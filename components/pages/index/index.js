ba4387349a0e1a7eeeb8f5689b2a9383f856b9c2
fouber <fouber.NET@gmail.com>
2014-07-16 21:19:28 +0800

    首页内容展示

--- components/pages/index/index.js
+++ components/pages/index/index.js
@@ -2,12 +2,59 @@
 
 module.exports = {
     getHTML: function () {
-        // tpl后缀的文件也可以用于模板嵌入，相比handlebars
-        // tpl文件不具有模板变量功能，嵌入后只是作为字符串使
-        // 用，tpl文件嵌入之前可以被插件压缩，体积更小。
-        // handlebars由于缺少相应的压缩插件因此暂时不能在预
-        // 编译阶段做压缩。选择tpl还是handlebars可以自由选
-        // 择
-        return __inline('index.tpl');
+        var data = [
+            {
+                title: '模块化开发',
+                icon: 'cubes',
+                href: '/#!/index'
+            },
+            {
+                title: '按需加载',
+                icon: 'archive',
+                href: '/#!/index'
+            },
+            {
+                title: '本地缓存',
+                icon: 'database',
+                href: '/#!/index'
+            },
+            {
+                title: '组件仓库',
+                icon: 'github',
+                href: '/#!/index'
+            },
+            {
+                title: '文件监听',
+                icon: 'eye',
+                href: '/#!/index'
+            },
+            {
+                title: '自动刷新',
+                icon: 'refresh',
+                href: '/#!/index'
+            },
+            {
+                title: '性能优化',
+                icon: 'leaf',
+                href: '/#!/index'
+            },
+            {
+                title: '代码校验',
+                icon: 'bug',
+                href: '/#!/index'
+            },
+            {
+                title: 'CSS雪碧图',
+                icon: 'th',
+                href: '/#!/index'
+            },
+            {
+                title: '工具扩展',
+                icon: 'puzzle-piece',
+                href: '/#!/index'
+            }
+        ];
+        var tpl = __inline('index.handlebars');
+        return tpl(data);
     }
 };
