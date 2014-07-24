14293916aa75392ee97fc682591e567a5f3ee25c
fouber <fouber.NET@gmail.com>
2014-07-24 16:55:53 +0800

    修改构建配置

--- .build.sh
+++ .build.sh
@@ -3,7 +3,7 @@
 if [ -d output ]; then
     echo "➥ Commit files"
     git clone --quiet --branch=deploy https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
-    cp -r output/* ${REPO_SLUG}
+    cp -r output/** ${REPO_SLUG}
     cd ${REPO_SLUG}
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
