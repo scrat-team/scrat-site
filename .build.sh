19b101d4c79dc3693ee3bac9eda444c8302e7b8e
fouber <fouber.NET@gmail.com>
2014-07-24 16:52:24 +0800

    修改构建配置

--- .build.sh
+++ .build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-cd output
-git config user.email "travis@travis-ci.org"
-git config user.name "travis-ci"
-git add -A
-git commit -m "[ci skip] publish website"
-git push -fq origin master > /dev/null
+if [ -d output ]; then
+    echo "➥ Commit files"
+    git clone --quiet --branch=deploy https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
+    cp -r output/* ${REPO_SLUG}
+    cd ${REPO_SLUG}
+    git config user.email "travis@travis-ci.org"
+    git config user.name "travis-ci"
+    git add -A
+    git commit -m "[ci skip] publish website"
+    git push -fq origin master > /dev/null
+else
+    echo '➥ Fail'
+    exit 1
+fi
